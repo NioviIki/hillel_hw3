016af113beb9ea94dd212d05ee018ffffbd244da
niovi <prodamgarazhnedorogo@gmail.com>
2023-01-18 02:38:54 +0200

    finally

---
+++ flaskr/sql_files/add.sql
@@ -0,0 +1,15 @@
+INSERT INTO all_genre (genre) VALUES ('punk');
+INSERT INTO all_genre (genre) VALUES ('rock');
+INSERT INTO all_genre (genre) VALUES ('metal');
+INSERT INTO all_genre (genre) VALUES ('exemple');
+INSERT INTO all_genre (genre) VALUES ('rap');
+
+INSERT INTO tracks (artist, title, length, genre_id) VALUES ('slipknot', 'wait and bleed', '00:02:48', '3');
+INSERT INTO tracks (artist, title, length, genre_id) VALUES ('soad', 'chop suey', '00:03:29', '3');
+INSERT INTO tracks (artist, title, length, genre_id) VALUES ('sex pistols', 'anarchy in the uk', '00:02:37', '1');
+INSERT INTO tracks (artist, title, length, genre_id) VALUES ('slipknot', 'Time To Sing', '00:04:31', '3');
+INSERT INTO tracks (artist, title, length, genre_id) VALUES ('jimi hendrix', 'all along the watchtower ', '00:02:02', '2');
+INSERT INTO tracks (artist, title, length, genre_id) VALUES ('exemple', 'exemple ', '01:12:59', '4');
+INSERT INTO tracks (artist, title, length, genre_id) VALUES ('eminem', 'Rap God ', '00:06:09', '5')
+
+
